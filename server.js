393164b6473252346adf1e9f20fecdc21f6036a0
amit.azoulay <amit.azoulay@is.com>
2021-10-06 18:39:26 +0300

    third commit, after issue resolving

--- server.js
+++ server.js
@@ -1,101 +1,138 @@
 const express=require('express')
 const app=express()
-const fs=require('fs')
+const fsPromises=require('fs').promises
 const port=3000
 let data=require('./data/podcasts.json')
 const bodyParser= require('body-parser')
 app.use(bodyParser.json())
-
+const joi = require('joi')
+const dbFileName='./data/podcasts.json'
+let idNumberToGive=1
 
 app.listen(port)
 
+function validateId(input) {
+  const schema= joi.object(
+      {
+        id: joi.number().integer().required(),
+      },
+  )
+  return schema.validate(input)
+}
 
-function filterById(id){
-    const result= data.filter(pod=>pod.id==id)
-    if(result.length===0){
-        return null
+function validateInput(input) {
+  const schema = joi.object().keys(
+      {
+        id: joi.number().integer().required().required(),
+        title: joi.string().required(),
+        description: joi.string().required(),
+        htmlDescription: joi.string().required(),
+        webUrl: joi.string().required(),
+        imageUrl: joi.string().required(),
+        language: joi.string().required(),
+        numberOfEpisodes: joi.number().integer().required(),
+        avgEpisodeLength: joi.number().integer().required(),
+        author: joi.string().required(),
+        category: joi.string().required(),
+      })
+  return schema.validate(input)
+}
+
+function filterById(id) {
+  const result= data.filter((podcast)=>podcast.id===id)
+  if (result.length===0) {
+    return null
+  }
+  return result
+}
+
+function writePodToDb(dataToBeSaved) {
+  try {
+    return fsPromises.writeFile(dbFileName, JSON.stringify(dataToBeSaved))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+async function deletePodcast(id) {
+  const updatedData=data.filter((podcast)=>podcast.id!== id)
+  await writePodToDb(updatedData)
+  data=updatedData
+}
+
+async function addNewPodcast(podcast) {
+  const updatedData=data
+  updatedData.push(podcast)
+  await writePodToDb(updatedData)
+  data=updatedData
+}
+
+async function updatePodcast(updatedPodcast) {
+  let podcastToUpdate=data.filter((podcast)=> podcast.id===updatedPodcast.id)[0]
+  podcastToUpdate={...podcastToUpdate, ...updatedPodcast}
+  const updatedPodcasts=data.filter((podcast)=>podcast.id!==podcastToUpdate.id)
+  updatedPodcasts.push(podcastToUpdate)
+  await writePodToDb(updatedPodcasts)
+  data=updatedPodcasts
+}
+
+
+app.get('/podcast/:id', (req, res)=>{
+  const validInputStatus=validateId(req.params)
+  if (validInputStatus.error) {
+    res.status(400).send('Please enter a numeric id')
+  } else {
+    const result = filterById(parseInt(req.params.id))
+    if (!result) {
+      res.status(404).send('This podcast does not exist')
+    } else {
+      res.status(200).send(result)
     }
-    return result
-}
-
-function writePodToDb(){
-    fs.writeFile('./data/podcasts.json',JSON.stringify(data), (err)=>{
-        if(err) {
-            throw err;
-        }
-    })
-}
-
-function deletePod(id){
-    data=data.filter(pod=>pod.id!= id)
-    writePodToDb()
-}
-
-function addNewPod(podcast){
-    data.push(podcast)
-    writePodToDb()
-}
-
-function updatePod(updatedPod){
-    let podToUpdate=data.filter(pod=> pod.id==updatedPod.id)[0]
-    podToUpdate={...podToUpdate,...updatedPod}
-    data=data.filter(pod=>pod.id!=podToUpdate.id)
-    data.push(podToUpdate)
-    writePodToDb()
-}
-
-
-app.get('/podcast/:id',(req,res)=>{
-    const id=req.params.id
-    if(!id){
-        res.status(400).send('No id entered')
-    }
-   const result=filterById(id)
-    res.send(result)
+  }
 })
 
-app.post('/podcast/new',(req,res)=>{
-    const id=filterById(req.body.id)
-    if(id){
-        res.status(400).send('This podcast already exists')
-    }
-    else{
-        addNewPod(req.body)
-        res.status(200).send('The podcast has been added')
-    }
+app.post('/podcast/new', async (req, res)=>{
+  req.body.id=idNumberToGive
+  idNumberToGive+=1
+  const validInputStatus=validateInput(req.body)
+  if (validInputStatus.error) {
+    res.status(400).send('Please make sure all fields are included in body')
+  }
+  await addNewPodcast(req.body)
+  res.status(200).send('The podcast has been added')
 })
 
 
-app.delete('/podcast/:id',(req,res)=>{
-    const id=req.params.id
-    if(!id){
-        res.status(400).send('No id entered')
+app.delete('/podcast/:id', async (req, res)=>{
+  const validInputStatus=validateId(req.params)
+  if (validInputStatus.error) {
+    res.status(400).send('Please enter a numeric id')
+  } else {
+    const result = filterById(parseInt(req.params.id))
+    if (!result) {
+      res.status(404).send('This podcast does not exist')
+    } else {
+      await deletePodcast(parseInt(req.params.id))
+      res.status(200).send('The item has been deleted successfully')
     }
-    const result=filterById(id)
-    if(!result){
-        res.status(404).send('This podcast does not exist, therefore cannot be deleted')
-    }
-    else {
-        deletePod(id)
-        res.status(200).send('The item has been deleted successfully')
-    }
+  }
 })
 
 
-app.put('/podcast/:id',(req,res)=>{
-    const id=req.params.id
-    if(!id){
-        res.status(400).send('No id entered')
+app.put('/podcast/:id', async (req, res)=>{
+  const validInputStatus=validateId(req.params)
+  if (validInputStatus.error) {
+    res.status(400).send('Please enter a numeric id')
+  } else {
+    const result = filterById(parseInt(req.params.id))
+    if (!result) {
+      res.status(404).send('This podcast does not exist')
+    } else {
+      await updatePodcast(req.body)
+      res.status(200).send('The item has been updated successfully')
     }
-    const result=filterById(id)
-    if(!result){
-        res.status(404).send('This podcast does not exist, therefore cannot be updated')
-    }
-    else {
-        updatePod(req.body)
-        res.status(200).send('The item has been updated successfully')
-    }
+  }
 })
 
 
-console.log('listening on port '+ port+ '...')
+console.log(`listening on port ${port}...`)
