a85e33a1bf1a625a6b99531f6306576c4599ecc7
amit.azoulay <amit.azoulay@is.com>
2021-10-13 10:30:55 +0300

    second commit, with added files

---
+++ schemas/podcast.js
@@ -0,0 +1,39 @@
+const joi = require('joi')
+
+const idSchema =
+     joi.object(
+       {
+         id: joi.number().integer().required()
+       }
+     )
+
+const updateSchema = joi.object().keys(
+  {
+    id: joi.number().integer(),
+    title: joi.string(),
+    description: joi.string(),
+    htmlDescription: joi.string(),
+    webUrl: joi.string(),
+    imageUrl: joi.string(),
+    language: joi.string(),
+    numberOfEpisodes: joi.number().integer(),
+    avgEpisodeLength: joi.number().integer(),
+    author: joi.string(),
+    category: joi.string()
+  })
+
+const addSchema = joi.object().keys(
+  {
+    title: joi.string().required(),
+    description: joi.string().required(),
+    htmlDescription: joi.string().required(),
+    webUrl: joi.string().required(),
+    imageUrl: joi.string().required(),
+    language: joi.string().required(),
+    numberOfEpisodes: joi.number().integer().required(),
+    avgEpisodeLength: joi.number().integer().required(),
+    author: joi.string().required(),
+    category: joi.string().required()
+  })
+
+module.exports = { addSchema, updateSchema, idSchema }
