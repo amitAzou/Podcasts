ea94c35bad82d5f56059d4b3aae250996c1ef8ed
amit.azoulay <amit.azoulay@is.com>
2021-10-20 17:34:25 +0300

    1. Fixed all of the issues of last commit regarding the component tests. 2. Added app.js and changed logic accordingly

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
-const express = require('express')
-const routes = require('./routes/index.js')
-const { getItemFromCache } = require('./middlewares/cacheMiddleware')
+const app = require('./app')
 const config = require('config')
-const bodyParser = require('body-parser')
-const app = express()
+
 app.listen(config.port)
 console.log(`listening on port ${config.port}...`)
-app.use(bodyParser.json())
-app.use(getItemFromCache)
-app.use(routes)
-
-module.exports = app
--- tests/componentTest/podcasts/mock.js
+++ tests/componentTest/podcasts/mock.js
@@ -38,4 +38,18 @@ const extraFieldsObject = {
   extraField: 'extra'
 }
 
-module.exports = { validObject, missingFieldsObject, extraFieldsObject }
+const wrongFieldTypesObject = {
+  title: 1,
+  description: 'My second test',
+  htmlDescription: '<p>Test.</p>',
+  webUrl: 'https://test.com',
+  imageUrl: 'https://ssl-static.libsyn.com/p/assets/e/0/7/9/e0790c69b0534a14/BGF_Podcast_Logo-2.png',
+  language: 2,
+  numberOfEpisodes: 9,
+  avgEpisodeLength: 2000,
+  author: 'Amit Azoulay',
+  category: 'Software',
+  extraField: 'extra'
+}
+
+module.exports = { validObject, missingFieldsObject, extraFieldsObject, wrongFieldTypesObject }
