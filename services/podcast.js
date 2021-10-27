81f174c933c5b51f51773d77d3d0101694c9a2b5
amit.azoulay <amit.azoulay@is.com>
2021-10-27 14:24:21 +0300

    fixed the "get best items" algorithm

--- services/podcast.js
+++ services/podcast.js
@@ -32,27 +32,28 @@ const getBestItems = async (number) => {
   const ratingArr = await getRatingsArr(reviewsArr)
   const result = []
   for (let i = 0; i < number && i < ratingArr.length; i++) {
-    result.push(dataBase.find((podcast) => podcast.id === ratingArr[i].id))
+    result.push(dataBase.find((podcast) => podcast.id === ratingArr[i][0]))
   }
   return result
 }
 
 const getRatingsArr = async (data) => {
-  let sum = data[0].rating
-  let counter = 1
-  const ratingArr = []
-  for (let i = 1; i < data.length; i++) {
-    if (data[i].podcastId === data[i - 1].podcastId) {
-      sum += data[i].rating
-      counter++
+  const ratingMap = new Map()
+  for (let i = 0; i < data.length; i++) {
+    const tempId = data[i].podcastId
+    if (ratingMap.has(data[i].podcastId)) {
+      const tempRatingObj = ratingMap.get(tempId)
+      const tempSum = tempRatingObj.sum
+      const tempCounter = tempRatingObj.counter
+      ratingMap.set(tempId, { sum: tempSum + data[i].rating, counter: tempCounter + 1 })
     } else {
-      ratingArr.push({ id: data[i - 1].podcastId, rating: sum / counter })
-      sum = data[i].rating
-      counter = 1
+      ratingMap.set(tempId, { sum: data[i].rating, counter: 1 })
     }
   }
-  ratingArr.push({ id: data[data.length - 1].podcastId, rating: sum / counter })
-  return ratingArr.sort((a, b) => b.rating - a.rating)
+  ratingMap.forEach((value) => {
+    value.avg = value.sum / value.counter
+  })
+  return [...ratingMap.entries()].sort((a, b) => b[1].avg - a[1].avg)
 }
 
 module.exports = {
