6c06e859ea83ed54ad7393d81f0419ba837c6b27
amit.azoulay <amit.azoulay@is.com>
2021-12-12 15:53:27 +0200

    First commit for this branch. Added the following: 1. React implentation of the add podcast page 2. Decoration fix of edit podcast page

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -7,10 +7,11 @@ import {
   Navigate,
 } from 'react-router-dom'
 import Podcasts from './containers/podcasts/podcasts'
-
 import PodcastDetails from './containers/podcast_details/Podcast_details'
 import EditPodcast from './containers/edit_podcast/EditPodcast'
 import AddPodcast from './containers/add_podcast/AddPodcast'
+import LoginPage from './containers/login_page/LoginPage'
+import './utils/axios'
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path="/" element={<Navigate to="/podcast" />} />
         <Route path="/podcast/edit-podcast/*" element={<EditPodcast />} />
         <Route path="/podcast/add/*" element={<AddPodcast />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   )
